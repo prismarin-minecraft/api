d349869eec88b1957e2b68dfb5fcb6c027a27e81
ReaperMaga <reapermaga@gmail.com>
2022-06-30 01:59:35 +0300

    Added not valid number message to PrismarinConstants

--- src/main/java/in/prismar/api/PrismarinConstants.java
+++ src/main/java/in/prismar/api/PrismarinConstants.java
@@ -15,6 +15,7 @@ public final class PrismarinConstants {
 
     /** Messages **/
     public static final String NO_PERMISSION_MESSAGE = PREFIX + "§cYou do not have enough permissions.";
+    public static final String NOT_VALID_NUMBER_MESSAGE = PREFIX + "§cPlease type a valid number.";
     public static final String PLAYER_NOT_FOUND_MESSAGE = PREFIX + "§7This player does not exists.";
     public static final String PLAYER_NOT_ONLINE_MESSAGE = PREFIX + "§7This player is not online.";
 
