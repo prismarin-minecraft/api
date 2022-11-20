034cb2d7dc758b168643922535d5032cb3f534dd
ReaperMaga <reapermaga@gmail.com>
2022-11-20 22:08:13 +0100

    Updated api

---
+++ src/main/java/in/prismar/api/warp/WarpProvider.java
@@ -0,0 +1,22 @@
+package in.prismar.api.warp;
+
+import in.prismar.api.PrismarinProvider;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+@PrismarinProvider
+public interface WarpProvider {
+
+    void teleport(Player player, String warp);
+
+    boolean existsWarp(String name);
+
+    Location getWarp(String name);
+
+}
