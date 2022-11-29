d66504ec8336a420816afed5a2ec15487f3e01d6
ReaperMaga <reapermaga@gmail.com>
2022-11-29 06:42:40 +0100

    Added region provider

---
+++ src/main/java/in/prismar/api/region/RegionProvider.java
@@ -0,0 +1,26 @@
+package in.prismar.api.region;
+
+import in.prismar.api.PrismarinProvider;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+@PrismarinProvider
+public interface RegionProvider {
+
+    boolean exists(String region);
+
+    boolean isIn(Location location, String region);
+
+    Map<String, Object> getFlag(String region, String flag);
+
+    boolean hasFlag(String region, String flag);
+
+}
