8bd49dd586def4254d53fc589fa06cd1e333a2c4
ReaperMaga <reapermaga@gmail.com>
2022-12-10 19:27:44 +0100

    Added teleport to spawn to WarpProvider

--- src/main/java/in/prismar/api/warp/WarpProvider.java
+++ src/main/java/in/prismar/api/warp/WarpProvider.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 public interface WarpProvider {
 
     void teleport(Player player, String warp);
+    void teleportToSpawn(Player player);
 
     boolean existsWarp(String name);
 
