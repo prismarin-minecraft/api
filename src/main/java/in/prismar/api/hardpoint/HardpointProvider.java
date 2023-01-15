ebc8d2d6bcfcf2187684f89c525eb69786620df7
ReaperMaga <reapermaga@gmail.com>
2023-01-15 23:16:28 +0100

    Added hardpoint

---
+++ src/main/java/in/prismar/api/hardpoint/HardpointProvider.java
@@ -0,0 +1,22 @@
+package in.prismar.api.hardpoint;
+
+import in.prismar.api.PrismarinProvider;
+import org.bukkit.entity.Player;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+@PrismarinProvider
+public interface HardpointProvider {
+
+    boolean isCurrentlyPlaying(Player player);
+
+    String getCurrentCapturedTeam();
+
+    long getPointRotation();
+
+    long getCurrentPoints(String team);
+}
