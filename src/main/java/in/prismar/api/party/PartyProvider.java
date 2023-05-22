d81791a31fea382d2a6eba4724fbf041f2f76e8a
ReaperMaga <reapermaga@gmail.com>
2023-05-22 07:17:50 +0200

    Added party to api

---
+++ src/main/java/in/prismar/api/party/PartyProvider.java
@@ -0,0 +1,25 @@
+package in.prismar.api.party;
+
+import in.prismar.api.PrismarinProvider;
+import in.prismar.library.common.event.EventBus;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+@PrismarinProvider
+public interface PartyProvider {
+
+    boolean isOwnerOfParty(Player player);
+
+    boolean hasParty(Player player);
+
+    List<Player> getPartyMembers(Player player);
+
+    EventBus getEventBus();
+}
