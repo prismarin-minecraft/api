4bd7a6c3b6fc50acb29e94d3097dbe12b9094593
ReaperMaga <reapermaga@gmail.com>
2023-06-05 19:42:23 +0200

    Added report provider

---
+++ src/main/java/in/prismar/api/report/ReportProvider.java
@@ -0,0 +1,15 @@
+package in.prismar.api.report;
+
+import in.prismar.api.PrismarinProvider;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+@PrismarinProvider
+public interface ReportProvider {
+
+    void deleteById(String id);
+}
