64b1adbbb54d106e517badbfc4d774f019b8f3d1
ReaperMaga <reapermaga@gmail.com>
2023-09-18 04:02:03 +0200

    Added collection exp booster type

--- src/main/java/in/prismar/api/booster/BoosterType.java
+++ src/main/java/in/prismar/api/booster/BoosterType.java
@@ -19,7 +19,8 @@ public enum BoosterType {
     MONEY(new ItemBuilder(Material.GOLD_INGOT).setName("§6Money").build(), 30 * 60),
     BATTLEPASS(new ItemBuilder(Material.NAME_TAG).setName("§dBattlepass").build(), 30 * 60),
     MINE(new ItemBuilder(Material.NETHERITE_PICKAXE).setName("§cMine").allFlags().build(), 30 * 60),
-    LUMBER(new ItemBuilder(Material.OAK_LOG).setName("§2Lumber").build(), 30 * 60);
+    LUMBER(new ItemBuilder(Material.OAK_LOG).setName("§2Lumber").build(), 30 * 60),
+    COLLECTION(new ItemBuilder(Material.PAINTING).setName("§aCollection XP").build(), 30 * 60);
 
     private final ItemStack icon;
     private final long untilInSeconds;
