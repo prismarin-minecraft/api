6f203175d8fb9b2cf8be25544f34aacb495c83e2
ReaperMaga <reapermaga@gmail.com>
2024-02-01 19:27:21 +0100

    feat: Added qol features to config store

--- src/main/java/in/prismar/api/configuration/ConfigStore.java
+++ src/main/java/in/prismar/api/configuration/ConfigStore.java
@@ -13,6 +13,26 @@ public interface ConfigStore {
 
     String getProperty(String key);
 
+    default int getIntProperty(String key) {
+        return Integer.parseInt(getProperty(key));
+    }
+
+    default double getDoubleProperty(String key) {
+        return Double.parseDouble(getProperty(key));
+    }
+
+    default long getLongProperty(String key) {
+        return Long.parseLong(getProperty(key));
+    }
+
+    default boolean getBooleanProperty(String key) {
+        return Boolean.parseBoolean(getProperty(key));
+    }
+
+    default float getFloatProperty(String key) {
+        return Float.parseFloat(getProperty(key));
+    }
+
     String getProperty(String category, String key);
 
 }
